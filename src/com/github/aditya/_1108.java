ab674ff90c7d77f7e220a81174a73485b433b640
adityagarde <aditya.garde96@gmail.com>
2021-06-04 23:34:50 +0530

    Initial Commit

---
+++ src/com/github/aditya/_1108.java
@@ -0,0 +1,9 @@
+package com.github.aditya;
+
+public class _1108 {
+    public static class Solution {
+        public String defangIPaddr(String address) {
+            return address.replace(".", "[.]");
+        }
+    }
+}
