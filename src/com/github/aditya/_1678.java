21edcdc1c2605e0bf0a7e57da0b79f197b37d695
adityagarde <aditya.garde96@gmail.com>
2021-06-10 23:58:15 +0530

    Without default replace() method

--- src/com/github/aditya/_1678.java
+++ src/com/github/aditya/_1678.java
@@ -3,12 +3,23 @@ package com.github.aditya;
 public class _1678 {
     public static class Solution {
         public String interpret(String command) {
-            return command.replace("(al)", "al").replace("()", "o");
-        }
 
-        public static void main(String[] args) {
-            Solution solution = new Solution();
-            System.out.println(solution.interpret("G()()()()(al)"));
+            StringBuffer sbf = new StringBuffer();
+            for (int i = 0; i < command.length(); i++) {
+                if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
+                    sbf.append('o');
+                    i++;
+                } else if (command.charAt(i) != '(' && command.charAt(i) != ')') {
+                    sbf.append(command.charAt(i));
+                }
+            }
+            return sbf.toString();
+        }
+    }
+
+    public static class Solution_1 {
+        public String interpret(String command) {
+            return command.replace("(al)", "al").replace("()", "o");
         }
     }
 }
