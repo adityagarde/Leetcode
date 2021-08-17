7d14a76a4c3a2999837e347e8c78fe755ff1acda
adityagarde <aditya.garde96@gmail.com>
2021-08-17 23:41:46 +0530

    _0704 : Updated Solution

--- src/com/github/aditya/_0704.java
+++ src/com/github/aditya/_0704.java
@@ -1,29 +1,46 @@
 package com.github.aditya;
 
 public class _0704 {
+    // 1. Recursive Approach
+    // 0 ms, faster than 100.00%, memory 39.4 MB, less than 98.50%
     public static class Solution {
         public int search(int[] nums, int target) {
-            return binarySearchRecursive(nums, target);
+            return binarySearchRecursive(nums, target, 0, nums.length - 1);
         }
 
-        public static int binarySearchRecursive(int[] arr, int x, int left, int right) {
+        public static int binarySearchRecursive(int[] arr, int target, int left, int right) {
             if (left > right) {
                 return -1;
             }
-
-            int mid = (left + right) / 2;
-            if (arr[mid] == x) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
                 return mid;
-            } else if (x > arr[mid]) {
-                return binarySearchRecursive(arr, x, mid + 1, right);
-            } else if (x < arr[mid]) {
-                return binarySearchRecursive(arr, x, left, mid - 1);
+            } else if (target > arr[mid]) {
+                return binarySearchRecursive(arr, target, mid + 1, right);
+            } else {
+                return binarySearchRecursive(arr, target, left, mid - 1);
+            }
+        }
+    }
+
+    // 2. Iterative Approach
+    // 0 ms, faster than 100.00%, memory 39.7 MB, less than 82.60%
+    public static class Solution_1 {
+        public int search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
             return -1;
-        }
-
-        public static int binarySearchRecursive(int[] arr, int x) {
-            return binarySearchRecursive(arr, x, 0, arr.length - 1);
         }
     }
 }
