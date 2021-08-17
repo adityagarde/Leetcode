364e33c02e804c7fed2cfd832b782f8269cd5ffa
adityagarde <aditya.garde96@gmail.com>
2021-08-18 01:08:49 +0530

    _0034 : Initial Commit

---
+++ src/com/github/aditya/_0034.java
@@ -0,0 +1,43 @@
+package com.github.aditya;
+
+public class _0034 {
+    // 0 ms, faster than 100.00%, Binary Search
+    // if we get the target number, store it as it may or may not be the first occurrence
+    // and set right = mid - 1 and search again.
+    // Similar for last occurrence, left = mid + 1 and search again.
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            int first = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) {
+                    first = mid;
+                    right = mid - 1;
+                } else if (target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            left = 0;
+            right = nums.length - 1;
+            int last = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) {
+                    last = mid;
+                    left = mid + 1;
+                } else if (target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return new int[]{first, last};
+        }
+    }
+}
