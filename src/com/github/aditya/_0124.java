5e35d4c0400dab63ff04e2d003b60b512c5034c6
adityagarde <aditya.garde96@gmail.com>
2022-05-03 15:21:37 +0530

    _0124 : Initial Commit

---
+++ src/com/github/aditya/_0124.java
@@ -0,0 +1,39 @@
+package com.github.aditya;
+
+public class _0124 {
+    // 1 ms, faster than 87.10%, memory 47.3 MB, less than 66.14%
+    class Solution {
+        int maxSum = Integer.MIN_VALUE;
+
+        public int maxPathSum(TreeNode root) {
+            maxSum(root);
+            return maxSum;
+        }
+
+        private int maxSum(TreeNode node) {
+            if (node == null)
+                return 0;
+
+            int left = Math.max(0, maxSum(node.left));
+            int right = Math.max(0, maxSum(node.right));
+
+            maxSum = Math.max(maxSum, left + right + node.val);
+
+            return node.val + Math.max(left, right);
+        }
+    }
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
