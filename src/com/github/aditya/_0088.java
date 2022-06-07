625eb696b785ebb4ddeca534971d38aa73864805
adityagarde <aditya.garde96@gmail.com>
2022-06-08 01:55:43 +0530

    _0088 : Update

--- src/com/github/aditya/_0088.java
+++ src/com/github/aditya/_0088.java
@@ -1,22 +1,19 @@
 package com.github.aditya;
 
 public class _0088 {
-    public static class Solution {
+    // 0 ms, faster than 100.00%, memory 39 MB less than 99.98%
+    // Time Complexity O(m+n) and Space Complexity O(1)
+    class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
             int i = m - 1, j = n - 1, k = m + n - 1;
-            while (i >= 0 && j >= 0) {
-                if (nums1[i] > nums2[j]) {
+            while (j >= 0) {
+                if (i >= 0 && nums1[i] > nums2[j]) {
                     nums1[k] = nums1[i];
                     i--;
                 } else {
                     nums1[k] = nums2[j];
                     j--;
                 }
-                k--;
-            }
-            while (j >= 0) {
-                nums1[k] = nums2[j];
-                j--;
                 k--;
             }
         }
