df87f5c83799a073b79aec7db1435e4648411c36
adityagarde <aditya.garde96@gmail.com>
2022-06-23 01:29:10 +0530

    _0215 : Update

--- src/com/github/aditya/_0215.java
+++ src/com/github/aditya/_0215.java
@@ -3,17 +3,16 @@ package com.github.aditya;
 import java.util.PriorityQueue;
 
 public class _0215 {
-    // 4 ms, faster than 78.74%, 44.9 MB, less than 43.84%
+    // 3 ms, faster than 87.76%, memory 42.4 MB, less than 89.99%
     class Solution {
         public int findKthLargest(int[] nums, int k) {
             PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-            for (int i = 0; i < k; i++) {
+            for (int i = 0; i < k; i++)
                 minHeap.add(nums[i]);
-            }
 
             for (int i = k; i < nums.length; i++) {
-                if (minHeap.peek() < nums[i]) {
+                if (nums[i] > minHeap.peek()) {
                     minHeap.poll();
                     minHeap.add(nums[i]);
                 }
