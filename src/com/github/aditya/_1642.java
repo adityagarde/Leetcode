b57038eca96a7ed9ee32ea9c88f68f144e66d483
adityagarde <aditya.garde96@gmail.com>
2022-06-23 05:12:22 +0530

    _1642 : Update

--- src/com/github/aditya/_1642.java
+++ src/com/github/aditya/_1642.java
@@ -6,17 +6,18 @@ public class _1642 {
     // 16 ms, faster than 97.94%, memory 58.6 MB, less than 84.88%
     // Time Complexity O(n * log L) and Space Complexity O(L)
 
-    /**
-     * Start with i = 1, calculate the diff with the previous height.
-     * We need resources only when diff > 0. Now, if ladders are available then add to the minHeap.
-     * If the diff (current climb) is larger than the smallest ladder distance (i.e. minHeap.peek() < diff),
-     * this means there is scope of optimisation as ladder should rather be used for the current diff,
-     * so replace the prev. ladder with the bricks.
-     * int bricksRequired = diff; => If there is no optimisation then anyways diff amount of bricks are required and if
-     * optimisation is there (as mentioned above) then new bricksRequired = prev ladder length i.e. minHeap.poll().
-     * Next we just check if we have enough bricks to complete this climb and continue (after subtraction) if YES,
-     * and return current height (i.e. i - 1) if NOT.
-     * return heights.length - 1 => Default case - we were able to climb till end using the resources given.
+    /*
+    1. Start with `i = 1`, calculate the diff with the previous height.
+    2. We need resources (ladders or bricks) only when `diff > 0`.
+    3. Now, if ladders are available then add to the minHeap `(minHeap.add(diff))`
+    4. If the `diff` (current climb) is larger than the smallest ladder distance (i.e. `minHeap.peek() < diff)`,
+       this means there is scope of optimisation as a ladder should rather be used for the current `diff` rather than bricks.
+    5. So replace the previous ladder with the bricks by putting `bricksRequired` as previous ladder length i.e. `minHeap.poll()`.
+    6. For the normal (non-optimised) case we have `int bricksRequired = diff`, as anyways `diff` number of bricks are required (will be consumed).
+    7. Next we just check if we have enough bricks to complete this climb.
+       If YES then we subtract the `bricksRequired` from the total bricks available and continue.
+    8. If we don't have enough bricks left this means we have already reached the farthest height so we return the current height i.e. `i -1`.
+    9. Default case is when we were able to climb and reach till the end using the resources given - so we simply return `heights.length - 1`.
      */
     class Solution {
         public int furthestBuilding(int[] heights, int bricks, int ladders) {
