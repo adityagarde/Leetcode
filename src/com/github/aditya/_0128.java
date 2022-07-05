5e9a78a8d0005f16583b8b8c462966d86a3cf8a1
adityagarde <aditya.garde96@gmail.com>
2022-07-06 03:05:20 +0530

    _0128 : Update

--- src/com/github/aditya/_0128.java
+++ src/com/github/aditya/_0128.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class _0128 {
+    // 25 ms, faster than 83.85%, memory 51.4 MB, less than 99.51%
+    // Time Complexity O(n) and Space Complexity O(n)
     class Solution {
         public int longestConsecutive(int[] nums) {
             int result = 0;
@@ -12,16 +14,21 @@ public class _0128 {
             for (int num : nums)
                 numSet.add(num);
 
-            for (int num : numSet) {
-                if (!numSet.contains(num - 1)) {
-                    int count = 1;
-                    int thisNum = num;
-                    while (numSet.contains(thisNum + 1)) {
-                        thisNum++;
-                        count++;
-                    }
-                    result = Math.max(count, result);
+            for (int num : nums) {
+                int count = 1;
+                int previous = num - 1, next = num + 1;
+
+                while (numSet.contains(previous)) {
+                    count++;
+                    numSet.remove(previous--);
                 }
+
+                while (numSet.contains(next)) {
+                    count++;
+                    numSet.remove(next++);
+                }
+
+                result = Math.max(result, count);
             }
             return result;
         }
