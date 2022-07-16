bb151515af0ad42e14e4c95100b389187252caf2
adityagarde <aditya.garde96@gmail.com>
2022-07-17 04:54:22 +0530

    _0105 : Update

--- src/com/github/aditya/_0105.java
+++ src/com/github/aditya/_0105.java
@@ -4,30 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class _0105 {
-    // 2 ms, faster than 99.03% and memory 42.2 MB, less than 88.53%
+    // 2 ms, faster than 99.03% and memory 41.9 MB, less than 95.00%
     // INORDER = Left Root Right && PREORDER = Root Left Right
     // PreOrder array will give the root - we chek this root's position in InOrder array (cached in Map)
     // Left part of this root's index in InOrder is the left subtree and right side is the right subtree.
     class Solution {
+        int current;
         public TreeNode buildTree(int[] preorder, int[] inorder) {
             Map<Integer, Integer> inMap = new HashMap<>();
 
             for (int i = 0; i < inorder.length; i++)
                 inMap.put(inorder[i], i);
 
-            return buildTree(preorder, new int[]{0}, 0, preorder.length - 1, inMap);
+            current = 0;
+            return buildTree(preorder, 0, preorder.length - 1, inMap);
         }
 
-        private TreeNode buildTree(int[] preOrder, int[] current, int start, int end, Map<Integer, Integer> inMap) {
-            if (current[0] >= preOrder.length || start > end)
+        private TreeNode buildTree(int[] preOrder, int start, int end, Map<Integer, Integer> inMap) {
+            if (current >= preOrder.length || start > end)
                 return null;
 
-            TreeNode root = new TreeNode(preOrder[current[0]]);
+            TreeNode root = new TreeNode(preOrder[current]);
+            current++;
 
-            current[0] += 1;
             int index = inMap.get(root.val);
-            root.left = buildTree(preOrder, current, start, index - 1, inMap);
-            root.right = buildTree(preOrder, current, index + 1, end, inMap);
+            root.left = buildTree(preOrder, start, index - 1, inMap);
+            root.right = buildTree(preOrder, index + 1, end, inMap);
 
             return root;
         }
